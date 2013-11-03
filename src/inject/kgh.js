f7bb843fdc6fac1c2d8665988558dbaacd45a0b3
Kyle Werner <kyle@wernull.com>
2013-11-03 09:47:21 -0500

    hide images on click

--- src/inject/kgh.js
+++ src/inject/kgh.js
@@ -4,6 +4,12 @@ kgh.InitKatoGifHide = function(){
 	$.get(chrome.extension.getURL("src/inject/kgh.html"), function(html){
 		$("body").append(html);
 	}, 'html');
+
+	$(".page-org").delegate('.preview-image','click',function(e) {
+        e.preventDefault();
+        $(e.target).closest('.message').find('.icon-collapse-alt').trigger('click');
+        return false;
+    });
 }
 
 chrome.extension.sendMessage({}, function(response) {
