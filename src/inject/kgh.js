f5a453ca2f1a8e1e40e7408a8344a82d5bc93ad9
Kyle Werner <kyle@wernull.com>
2013-11-03 14:53:37 -0500

    hook up flyout events

--- src/inject/kgh.js
+++ src/inject/kgh.js
@@ -3,13 +3,44 @@ kgh = {};
 kgh.InitKatoGifHide = function(){
 	$.get(chrome.extension.getURL("src/inject/kgh.html"), function(html){
 		$("body").append(html);
+    	kgh.SetFlyoutEvents();
 	}, 'html');
 
+	// trigger message collapse when image is clicked
 	$(".page-org").delegate('.preview-image','click',function(e) {
         e.preventDefault();
         $(e.target).closest('.message').find('.icon-collapse-alt').trigger('click');
-        return false;
     });
+
+}
+
+kgh.SetFlyoutEvents = function () {
+	// hide all items of category clicked in flyout
+	$(".kgh-hide").on('click',function(e) {
+        e.preventDefault();
+
+        var closeMessage = null;
+
+        switch($(e.target).data("hide")) {
+        	case "images":
+        		closeMessage = $('.preview-image').closest('.message');
+        		break;
+        	case "video":
+        		closeMessage = $('.preview-video').closest('.message');
+        		break;
+        	case "text":
+        		closeMessage = $('.message:not(:has(.preview-image, .preview-video))');
+        		break;
+        	case "all":
+        		closeMessage = $('.message');
+        		break;
+        }
+
+        closeMessage.find('.icon-collapse-alt').trigger('click');
+
+    });
+
+
 }
 
 chrome.extension.sendMessage({}, function(response) {
